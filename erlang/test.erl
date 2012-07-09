9e078d2e8428a74370dbaf0c45b91e86a6f2c63f
oujinliang <oujinliang@gmail.com>
2012-07-09 10:40:45 +0800

    add split_words

--- erlang/test.erl
+++ erlang/test.erl
@@ -1,3 +1,7 @@
+-module(test).
+-compile([export_all]).
+
+%% Count words in a string.
 count_words(Str)  ->
     count_words(Str, " ,;.").
 count_words(Str, Separators)  ->                                                                                                                                              
@@ -7,7 +11,20 @@ count_words(Str, Separators)  ->
              {false, true} -> {InWord, C + 1};
              _ -> {InWord, C}
           end
-       end,
-       {false, 0},
-       Str),
+       end, {false, 0}, Str),
     Count.
+
+%% Split string to words
+split_words(Str) ->
+    split_words(Str, " ,;.").
+split_words(Str, Separators) ->
+    split_words(Str, 0, [[]], false, Separators).
+split_words([], Count, Words, _PreInWord, _Separators) ->
+    {Count, lists:reverse( [ lists:reverse(Word) || Word <- Words, Word /= []] )};
+split_words([Ch | Rest], Count, [Current | RestWords] = Words, PreInWord, Separators) ->
+    InWord = not lists:member(Ch, Separators),
+    case {PreInWord, InWord} of 
+        {false, true} -> split_words(Rest, Count + 1, [[Ch] | Words], InWord, Separators);
+        {true, true}  -> split_words(Rest, Count, [[Ch|Current] | RestWords], InWord, Separators);
+        _             -> split_words(Rest, Count, Words, InWord, Separators)
+    end. 
